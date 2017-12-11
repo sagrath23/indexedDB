6cfadca2d6155c8ac4a51dbd8038fc77092fd1e0
Steven Villegas <sagrath23@gmail.com>
2017-12-11 06:57:43 -0500

    fixed IAsyncStoreSpec.ts

--- src/interfaces/IAsyncStorage.ts
+++ src/interfaces/IAsyncStorage.ts
@@ -5,7 +5,6 @@ export interface IAsyncStorage {
     get(objectStoreName: string, keyValue: IDBKeyRange | IDBValidKey) : Promise<Object>
     put(objectStoreName: string, item: any, keyValue?: IDBKeyRange | IDBValidKey)
     delete(objectStoreName: string, keyValue: IDBKeyRange | IDBValidKey)
-    openCursor(objectStoreName: string, cursorRange?: IDBKeyRange | IDBValidKey, cursorDirection?: IDBCursorDirection): Promise<IDBCursor>
-    index(objectStoreName: string, key: IDBKeyRange | IDBValidKey)
     getItemByIndex(objectStoreName: string, keyName: string, keyValue: IDBKeyRange | IDBValidKey): Promise<Object>
+    getItemsByCursor(objectStoreName: string, cursorRange?: IDBKeyRange | IDBValidKey, cursorDirection?: IDBCursorDirection): Promise<Object[]>
 }
